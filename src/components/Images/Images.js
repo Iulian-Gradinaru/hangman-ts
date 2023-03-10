3c85bf5cd01793d827da2fbba1d107d61debffea
unknown <valy_empire@yahoo.com>
2023-03-10 20:17:15 +0200

    last-updates

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import img0 from '../Images/0.jpg';
-import img1 from '../Images/1.jpg';
-import img2 from '../Images/2.jpg';
-import img3 from '../Images/3.jpg';
-import img4 from '../Images/4.jpg';
-import img5 from '../Images/5.jpg';
-import img6 from '../Images/6.jpg';
+import React from "react";
+import img0 from "../Images/0.jpg";
+import img1 from "../Images/1.jpg";
+import img2 from "../Images/2.jpg";
+import img3 from "../Images/3.jpg";
+import img4 from "../Images/4.jpg";
+import img5 from "../Images/5.jpg";
+import img6 from "../Images/6.jpg";
 
 const Image = [img0, img1, img2, img3, img4, img5, img6];
 
@@ -13,7 +13,7 @@ export const Images = (props) => {
   const { numberOfMistakes } = props;
   return (
     <div>
-      <img src={Image[numberOfMistakes]} alt={'img'} />
+      <img src={Image[numberOfMistakes]} alt={"img"} />
     </div>
   );
 };
--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export const Word = (props) => {
-  const { wordToGuess, lettersPressed } = props;
-  const formatLetter = (letter) => {
-    return lettersPressed.includes(letter) ? letter : '_';
-  };
+  const { wordToGuess, formatLetter } = props;
 
   const renderWord = () => {
-    return wordToGuess.split('').map((letter, index) => {
-      return <span key={index}>{formatLetter(letter)}</span>;
+    return wordToGuess.split("").map((letter, index) => {
+      return (
+        <span style={{ letterSpacing: "0.75em" }} key={index}>
+          {formatLetter(letter, index)}
+        </span>
+      );
     });
   };
 
