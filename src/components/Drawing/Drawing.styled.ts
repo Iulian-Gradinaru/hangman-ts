5d878fd35fe09b0622b431a93800f808a1166479
IulianGradinaru <iulian_gradinaru@yahoo.com>
2023-03-28 08:57:08 +0300

    modified components Drawing

--- src/components/Drawing/Drawing.styled.ts
+++ src/components/Drawing/Drawing.styled.ts
@@ -10,7 +10,6 @@ export const DrawingContainer = styled('img')(() => {
   return {
     maxHeight: 260,
     width: 'auto',
-
     border: '10px solid white',
     boxShadow: '0 10px 30px rgba(0, 0, 0, 0.27)',
   };
